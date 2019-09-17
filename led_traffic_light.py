2f8389c14f9a479b6d1f3ca55de4bf5227742dff
Yasin Ariffin <yasin.ariffin@gmail.com>
2019-09-18 00:23:08 +0800

    Updated version

--- led_traffic_light.py
+++ led_traffic_light.py
@@ -1,6 +1,4 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> from gpiozero import LED
+from gpiozero import LED
 from time import sleep
 red = LED(25)
 amber = LED(8)
